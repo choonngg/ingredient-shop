0c3b68af045896591bebc1e589b12aa2b94cdb2f
maenggyuno <mgo0415@naver.com>
2025-02-24 00:17:23 +0900

    Remove node_modules from repository

---
+++ src/App.js
@@ -0,0 +1,80 @@
+
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import LoginPage from './pages/LoginPage';
+// import MainDashboard from './pages/MainDashboard';
+// import MartDashboard from './pages/MartDashboard';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<LoginPage />} />
+//         <Route path="/dashboard" element={<MainDashboard />} />
+//         <Route path="/mart/:martId" element={<MartDashboard />} />
+//       </Routes>
+//       <Footer />
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+
+
+// import React, { useState } from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import LoginPage from './pages/LoginPage';
+// import MainDashboard from './pages/MainDashboard';
+// import MartDashboard from './pages/MartDashboard';
+
+
+// const App = () => {
+//   const [isAuthenticated, setIsAuthenticated] = useState(true); // 일단 기본적으로 인증 상태를 true로 설정
+
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<LoginPage setIsAuthenticated={setIsAuthenticated} />} />
+//         <Route path="/dashboard" element={<MainDashboard />} />
+//         <Route path="/mart/:martId" element={<MartDashboard />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import MainDashboard from './pages/MainDashboard';
+import MartDashboard from './pages/MartDashboard';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(true); // 기본적으로 인증 상태 true
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LoginPage setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/dashboard" element={<MainDashboard />} />
+        <Route path="/mart/:martId" element={<MartDashboard />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
+
+
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <p>&copy; 2025 유통기한 임박 상품 관리 시스템</p>
+      </footer>
+    );
+  };
+  export default Footer;
